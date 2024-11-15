fd75c6f8e9f3b766a5916bc4b829f222435302c1
esraisik <isikesra01@gmail.com>
2024-11-15 11:40:51 +0300

    first commit

---
+++ src/day01/C02.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class C02 {
+
+    public static void main(String[] args) {
+        int a=128;
+        int b =17;
+
+        System.out.println("c= " + (a+b));
+
+    }
+}
