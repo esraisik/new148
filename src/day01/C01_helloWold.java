65e6c27195028966ad0a4b41b57cf17994490e47
esraisik <isikesra01@gmail.com>
2024-11-15 11:44:43 +0300

    second commit

--- src/day01/C01_helloWold.java
+++ src/day01/C01_helloWold.java
@@ -4,5 +4,7 @@ public class C01_helloWold {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Hello Github");
     }
 }
--- src/day01/C02.java
+++ src/day01/C02.java
@@ -1,5 +1,7 @@
 package day01;
 
+import java.util.Scanner;
+
 public class C02 {
 
     public static void main(String[] args) {
@@ -7,6 +9,18 @@ public class C02 {
         int b =17;
 
         System.out.println("c= " + (a+b));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bir sayi giriniz  :  ");
+
+        int sayi = scanner.nextInt();
+
+        if (sayi %2==0) {
+            System.out.println("Girdiğiniz sayı bir çift sayıdır");
+        } else {
+            System.out.println("girdiğiniz sayı tek sayıdır");
+        }
 
     }
 }
